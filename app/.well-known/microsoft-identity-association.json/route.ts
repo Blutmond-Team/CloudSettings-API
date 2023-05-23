60838623a74f4e988a5fe39d755156214fda0d4c
Christian Raoulis <CRaoulis@googlemail.com>
2023-05-23 14:07:37 +0200

    fix microsoft azure domain verification

--- app/.well-known/microsoft-identity-association.json/route.ts
+++ app/.well-known/microsoft-identity-association.json/route.ts
@@ -1,9 +1,11 @@
-export function GET() {
-    return {
+import {NextResponse} from "next/server";
+
+export async function GET(request: Request) {
+    return NextResponse.json({
         "associatedApplications": [
             {
                 "applicationId": "79bb8b12-5f76-41f9-b0df-2489df28cf94"
             }
         ]
-    }
+    });
 }
