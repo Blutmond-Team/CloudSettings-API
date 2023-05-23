1fc9163dcf1e572c1e851431030ebf627250380f
Christian Raoulis <CRaoulis@googlemail.com>
2023-05-23 14:02:18 +0200

    add microsoft azure domain verification

---
+++ app/.well-known/microsoft-identity-association.json/route.ts
@@ -0,0 +1,9 @@
+export function GET() {
+    return {
+        "associatedApplications": [
+            {
+                "applicationId": "79bb8b12-5f76-41f9-b0df-2489df28cf94"
+            }
+        ]
+    }
+}
