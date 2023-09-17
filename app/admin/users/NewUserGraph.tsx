fca5942f6a5e965292d26dbeeff0e24186d36c7a
Christian Raoulis <CRaoulis@googlemail.com>
2023-09-17 16:07:25 +0200

    show unverified users in NewUserGraph.tsx and TotalUserGraph.tsx

--- app/admin/users/NewUserGraph.tsx
+++ app/admin/users/NewUserGraph.tsx
@@ -15,19 +15,21 @@ export const NewUserGraph = ({data}: Props) => {
         const firstEntry = seperatedArray[0];
         const lastEntry = seperatedArray[seperatedArray.length - 1];
 
-        const resultData: { key: string, count: number }[] = [];
+        const resultData: { key: string, count: number, unverified: number }[] = [];
         for (let date = firstEntry[0].jointAt.setHours(0, 0, 0, 0); date < lastEntry[0].jointAt.setHours(0, 0, 0, 0); date += 24 * 60 * 60 * 1000) {
             const seperatedDataEntry = seperatedData[date];
             const key = new Date(date).toLocaleDateString();
             if (!seperatedDataEntry || seperatedDataEntry.length === 0) {
                 resultData.push({
                     key: key,
-                    count: 0
+                    count: 0,
+                    unverified: 0
                 });
             } else {
                 resultData.push({
                     key: key,
-                    count: seperatedDataEntry.length
+                    count: seperatedDataEntry.length,
+                    unverified: seperatedDataEntry.filter(value => !value.verified).length
                 });
             }
         }
@@ -44,13 +46,19 @@ export const NewUserGraph = ({data}: Props) => {
                 height={384}
             >
                 <CartesianGrid strokeDasharray={"3 3"}/>
-                <YAxis dataKey={"count"}/>
+                <YAxis/>
                 <XAxis dataKey={"key"}/>
                 <Area
                     dataKey={"count"}
                     type={"monotone"}
                     stroke={"#8884d8"}
                     fill={"#8884d8"}
+                />
+                <Area
+                    dataKey={"unverified"}
+                    type={"monotone"}
+                    stroke={"#f04eff"}
+                    fill={"#f04eff"}
                 />
                 <Tooltip
                     wrapperClassName={"!bg-white dark:!bg-pale-800"}
--- app/admin/users/TotalUserGraph.tsx
+++ app/admin/users/TotalUserGraph.tsx
@@ -1,7 +1,7 @@
 "use client"
 import {UserData} from "@/app/admin/users/page";
-import {Area, AreaChart, CartesianGrid, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
-import * as _ from "lodash"
+import {Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis} from "recharts";
+import _ from "lodash";
 import {useMemo} from "react";
 
 type Props = {
@@ -15,21 +15,25 @@ export const TotalUserGraph = ({data}: Props) => {
         const firstEntry = seperatedArray[0];
         const lastEntry = seperatedArray[seperatedArray.length - 1];
 
-        const resultData: { key: string, count: number }[] = [];
+        const resultData: { key: string, count: number, unverified: number }[] = [];
         let currentUserCount = 0;
+        let unverifiedUserCount = 0;
         for (let date = firstEntry[0].jointAt.setHours(0, 0, 0, 0); date < lastEntry[0].jointAt.setHours(0, 0, 0, 0); date += 24 * 60 * 60 * 1000) {
             const seperatedDataEntry = seperatedData[date];
             const key = new Date(date).toLocaleDateString();
             if (!seperatedDataEntry || seperatedDataEntry.length === 0) {
                 resultData.push({
                     key: key,
-                    count: currentUserCount
+                    count: currentUserCount,
+                    unverified: unverifiedUserCount
                 });
             } else {
                 currentUserCount += seperatedDataEntry.length;
+                unverifiedUserCount += seperatedDataEntry.filter(value => !value.verified).length;
                 resultData.push({
                     key: key,
-                    count: currentUserCount
+                    count: currentUserCount,
+                    unverified: unverifiedUserCount
                 });
             }
         }
@@ -46,13 +50,19 @@ export const TotalUserGraph = ({data}: Props) => {
                 height={384}
             >
                 <CartesianGrid strokeDasharray={"3 3"}/>
-                <YAxis dataKey={"count"}/>
+                <YAxis/>
                 <XAxis dataKey={"key"}/>
                 <Area
                     dataKey={"count"}
                     type={"monotone"}
                     stroke={"#8884d8"}
                     fill={"#8884d8"}
+                />
+                <Area
+                    dataKey={"unverified"}
+                    type={"monotone"}
+                    stroke={"#f04eff"}
+                    fill={"#f04eff"}
                 />
                 <Tooltip
                     wrapperClassName={"!bg-white dark:!bg-pale-800"}
